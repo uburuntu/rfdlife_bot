fd46e146b33c9a83a78331b70a29aa2bf42b280e
uburuntu <bekbulatov.ramzan@ya.ru>
2018-06-29 19:38:20 +0300

    new: retry at telegram api exception

--- utils/telebot_wrapper.py
+++ utils/telebot_wrapper.py
@@ -1,8 +1,30 @@
 # _*_ coding: utf-8 _*_
 from datetime import datetime
 
+import functools
 import telebot
 from telebot.apihelper import ApiException
+
+
+def retry(exception, retries_count=2):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            ret, exc = None, None
+            for attempt in range(retries_count):
+                try:
+                    ret = func(*args, **kwargs)
+                except exception as e:
+                    exc = e
+                else:
+                    break
+            else:
+                TelebotWrapper.log(f'function {func.__name__}; {exc}')
+            return ret
+
+        return wrapped
+
+    return my_decorator
 
 
 class TelebotWrapper(telebot.TeleBot):
@@ -12,15 +34,19 @@ class TelebotWrapper(telebot.TeleBot):
 
     @staticmethod
     def log(text):
-        print('Telegram api exception: {}\n{}\n'.format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'), text))
+        print('{}\nTelegram api exception: {}\n'.format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'), text))
 
     def init_name(self):
         self.name = '@' + self.get_me().username
 
+    @retry(ApiException)
     def send_message(self, chat_id, text, disable_web_page_preview=None, reply_to_message_id=None, reply_markup=None,
                      parse_mode=None, disable_notification=None):
-        try:
-            return super().send_message(chat_id, text, disable_web_page_preview, reply_to_message_id, reply_markup,
-                                        parse_mode, disable_notification)
-        except ApiException as e:
-            self.log(f'{e},\nchat_id={chat_id}, text=({text})')
+        return super().send_message(chat_id, text, disable_web_page_preview, reply_to_message_id, reply_markup,
+                                    parse_mode, disable_notification)
+
+    @retry(ApiException)
+    def edit_message_text(self, text, chat_id=None, message_id=None, inline_message_id=None, parse_mode=None,
+                          disable_web_page_preview=None, reply_markup=None):
+        return super().edit_message_text(text, chat_id, message_id, inline_message_id, parse_mode,
+                                         disable_web_page_preview, reply_markup)
