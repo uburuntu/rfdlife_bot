d4e5586eaa07803544c097b80be3be0c5c17bc6e
@bergmanyo <35969814+bergmanyo@users.noreply.github.com>
2022-09-03 16:07:59 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -2,7 +2,7 @@ import os
 
 # Set your api tokens and keys through environmental variables
 # (add lines to your .bashrc and restart terminal):
-# export RFDLIFE_BOT_TOKEN='2085221709:AAFEhsjTQdUjN6MIOYYc3UadLhRBg0JTOgA'
+# export RFDLIFE_BOT_TOKEN=''
 # export RFDLIFE_BOT_AUTH_LOGIN='first_name.second_name'
 # export RFDLIFE_BOT_AUTH_PSWD='corp_password'
 # export RFDLIFE_BOT_ACCESS_PSWD='registration_password'
@@ -16,16 +16,16 @@ import os
 # [ Required ]
 
 default_bot = ''
-bot = os.getenv('RFDLIFE_BOT_TOKEN', default_bot)
+bot = os.getenv('5642719790:AAFoQK-GnzbBTvzKy3s7uYgN7lvN-zvdKeA', default_bot)
 
 default_auth_login = ''
-auth_login = os.getenv('RFDLIFE_BOT_AUTH_LOGIN', default_auth_login)
+auth_login = os.getenv('@rfd_nn_bot', default_auth_login)
 
 default_auth_pswd = ''
-auth_pswd = os.getenv('RFDLIFE_BOT_AUTH_PSWD', default_auth_pswd)
+auth_pswd = os.getenv('eJVW91qP', default_auth_pswd)
 
 default_access_pswd = ''
-access_pswd = os.getenv('RFDLIFE_BOT_ACCESS_PSWD', default_access_pswd)
+access_pswd = os.getenv('eJVW91qP', default_access_pswd)
 
 # [ Optional ]
 
