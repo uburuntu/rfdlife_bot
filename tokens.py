ebff95846deb9a221e9dc17d69b2bfd3d35a271d
@bergmanyo <35969814+bergmanyo@users.noreply.github.com>
2022-09-03 15:51:27 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -16,16 +16,16 @@ import os
 # [ Required ]
 
 default_bot = ''
-bot = os.getenv('RFDLIFE_BOT_TOKEN', default_bot)
+bot = os.getenv('5642719790:AAFoQK-GnzbBTvzKy3s7uYgN7lvN-zvdKeA', default_bot)
 
 default_auth_login = ''
-auth_login = os.getenv('RFDLIFE_BOT_AUTH_LOGIN', default_auth_login)
+auth_login = os.getenv('@rfd_nn_bot', default_auth_login)
 
 default_auth_pswd = ''
-auth_pswd = os.getenv('RFDLIFE_BOT_AUTH_PSWD', default_auth_pswd)
+auth_pswd = os.getenv('eJVW91qP', default_auth_pswd)
 
 default_access_pswd = ''
-access_pswd = os.getenv('RFDLIFE_BOT_ACCESS_PSWD', default_access_pswd)
+access_pswd = os.getenv('eJVW91qP', default_access_pswd)
 
 # [ Optional ]
 
